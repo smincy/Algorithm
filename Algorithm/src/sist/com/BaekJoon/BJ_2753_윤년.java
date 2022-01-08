b94dc14ac35b08059102ba05825ed89837218bdf
smincy@naver.com <smincy@naver.com>
2022-01-08 11:51:13 +0900

    백준2753_윤년

---
+++ Algorithm/src/sist/com/BaekJoon/BJ_2753_윤년.java
@@ -0,0 +1,21 @@
+package sist.com.BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ_2753_À±³â {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		if (N % 4 == 0 && N % 100 != 0 || N % 400 == 0) {
+			System.out.println(1);
+		}else {
+			System.out.println(0);
+		}
+	}
+}
