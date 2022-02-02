dd125a597075edc02b797edcc66892cb461c54e0
smincy <smincy@naver.com>
2022-02-02 17:17:39 +0900

    백준18108_1998년생

---
+++ Algorithm/src/sist/com/BaekJoon/BJ_18108_1998년생.java
@@ -0,0 +1,19 @@
+package sist.com.BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ_18108_1998년생 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		System.out.println(N - 543);
+			
+	}
+
+}
