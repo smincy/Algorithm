4440028fa8ce227e7ea1893c265255b222cfa8f4
walnut <smincy@naver.com>
2022-11-26 17:23:50 +0900

    백준

---
+++ Algorithm/src/sist/com/BaekJoon/BJ_14465_소가길을건너간이유5.java
@@ -0,0 +1,19 @@
+package Algorithm.src.sist.com.BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ_14465_소가길을건너간이유5 {
+    // 횡단보도 N 개
+    // 연속한 K개의 신호등
+    // 고장난 신호등의 개수 B
+    // B개의 고장난 신호등의 번호가 순서대로
+    // 연속 K개의 신호등이 존재하려면 최소 몇 개의 신호등을 수리해야 하는가
+
+    static int N, K, B;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
