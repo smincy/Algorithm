d503964586b91af9b690c7c1b52a443572e58efc
성민 <htomg@aromit.com>
2023-06-17 12:37:12 +0900

    백준 - 2435 - 기상청 인턴 신현수

---
+++ Algorithm/src/sist/com/BaekJoon/BJ_2435_기상청인신현수.java
@@ -0,0 +1,38 @@
+package Algorithm.src.sist.com.BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+import java.util.StringTokenizer;
+
+public class BJ_2435_기상청인신현수 {
+    static int N, K;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            int tmp = 0;
+            for (int j = i; j < i + K; j++) {
+                if (j >= arr.length){
+                    tmp = Integer.MIN_VALUE;
+                    break;
+                }
+                tmp += arr[j];
+            }
+            max = Math.max(tmp, max);
+        }
+        System.out.println(max);
+    }
+}
