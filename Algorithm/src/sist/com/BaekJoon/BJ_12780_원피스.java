7be8005d79788e79fd238f414339dd23341caf78
성민 <htomg@aromit.com>
2023-07-14 17:03:57 +0900

    백준 - 12780 - 원피스

---
+++ Algorithm/src/sist/com/BaekJoon/BJ_12780_원피스.java
@@ -0,0 +1,31 @@
+package Algorithm.src.sist.com.BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ_12780_원피스 {
+    static String str, target;
+    static int cnt;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        str = br.readLine();
+        target = br.readLine();
+
+        char tmp = target.charAt(0);
+        cnt = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == tmp) {
+                strCheck(i);
+            }
+        }
+        System.out.println(cnt);
+    }
+
+    public static void strCheck(int a) {
+        if (a + target.length() <= str.length() && str.substring(a, a + target.length()).equals(target)) {
+            cnt++;
+        }
+    }
+}
